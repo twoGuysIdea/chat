8802e17a695d329aef6f54eaa7a0bfbd5337debd
yangyiwei <yangyiwei@LAPTOP-V8JGD5NF>
2018-06-07 11:07:19 +0800

    =====

--- chat-web-service/src/main/java/com/weige/ssm/config/SwaggerConfig.java
+++ chat-web-service/src/main/java/com/weige/ssm/config/SwaggerConfig.java
@@ -10,9 +10,14 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-//@Configuration
-//@EnableSwagger2
+/**
+ * swagger配置
+ * @author yangyiwei
+ * @date 2018年6月7日
+ * @time 上午11:06:55
+ */
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
 	@Bean
